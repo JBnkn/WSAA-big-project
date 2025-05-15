8ec12fba50c6dc86cd9eec23efe1aba97f61275f
Joseph Benkanoun <benkanoun.greene@gmail.com>
2025-05-15 20:39:28 +0100

    Rest Server

--- wsaarestserver.py
+++ wsaarestserver.py
@@ -11,4 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return "Hello world"
+        return "Building my REST server"
+
+if __name__ == "__main__":
+    app.run(debug = True)
