ab7da4b6bc51628e75bcb98a5dd8d7f91849a908
Joseph Benkanoun <benkanoun.greene@gmail.com>
2025-05-19 21:32:56 +0100

    JSON get from last.fm

--- lastfm/lastfm-artists.py
+++ lastfm/lastfm-artists.py
@@ -2,4 +2,18 @@ import requests
 import mysql.connector
 import lastfmapi as api
 
-api_key = 
+api_key = api.lastfm['api']
+
+conn = mysql.connector.connect(
+    host= api.mysql['host'],
+    user= api.mysql['user'],
+    password= api.mysql['password'],
+    database=api.mysql['database']
+)
+cursor = conn.cursor()
+
+topartists = f"http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key={api_key}&format=json"
+response = requests.get(topartists)
+data = response.json()
+
+print(data)
